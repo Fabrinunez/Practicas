10ab9ac6ddd9bdd078fce6b31985b3b20aa948cf
fgutierrez <f.gutierrez021@ebc.edu.mx>
2026-08-13 22:03:07 -0600

    subiendo proyecto completo 13 agosto

---
+++ pedidos/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Producto, Pedido
+
+admin.site.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'precio', 'categoria', 'disponible')
+    list_filter = ('categoria', 'disponible')
+    search_fields = ('nombre',)
+
+admin.site.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cliente_nombre', 'estado', 'total', 'fecha')
+    list_filter = ('estado', 'fecha')
+    search_fields = ('cliente_nombre',)
