65fb74be10294aa425cfe29ba3a349623376b2ff
fgutierrez <f.gutierrez021@ebc.edu.mx>
2026-08-20 20:31:35 -0600

    Guardando mi progreso

--- cafeteriafabricio/settings.py
+++ cafeteriafabricio/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #middleware personalizado para auditoria de seguridad
+    'pedidos.middleware.AuditoriaSeguridadMiddleware',
 ]
 
 ROOT_URLCONF = 'cafeteriafabricio.urls'
@@ -122,6 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+#configuraciones de los archivos de media file
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # Email
